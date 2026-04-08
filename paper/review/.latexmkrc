d005cd63084529c38d46a7bc247aa0cb193e86ca
pengkangzhen <pengkangzhen@outlook.com>
2026-04-08 20:08:36 +0800

    chore: remove paper/ from tracking and add to .gitignore

--- paper/review/.latexmkrc
+++
@@ -1,2 +0,0 @@
-$pdflatex = 'xelatex -synctex=1 -interaction=nonstopmode -file-line-error %O %S';
-$pdf_mode = 1;
--- paper/review/filter_toc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Filter subsubsection entries from .toc file
-cd "$(dirname "$0")"
-if [ -f response_letter.toc ]; then
-    grep -v "subsubsection" response_letter.toc > response_letter.toc.tmp
-    mv response_letter.toc.tmp response_letter.toc
-fi
